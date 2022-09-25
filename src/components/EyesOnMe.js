0b8f81f618aabd234059aa7767c352a84cadf3ca
chrischep <christine.chepleting@student.moringaschool.com>
2022-09-25 14:17:17 +0300

    lab completed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React from "react";
+function EyesOnMe(){
+    const handleBlur=(event)=>{
+        console.log("Hey! Eyes on me!")
+    }
+    const handleFocus=()=>{
+        console.log("Good!")
+    }
+    return(
+         <div>
+            <button onBlur={handleBlur}onFocus={handleFocus}>
+                Eyes on me
+            </button>
+         </div>
+    )
+    
+} 
+export default EyesOnMe;
